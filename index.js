fc199134ab967eba965c56ced3dbfa1699f00320
cule219 <culaficstefan@gmail.com>
2019-08-12 15:33:15 +0200

    data moved to arrays

--- index.js
+++ index.js
@@ -1,41 +1,71 @@
+const tableHeadData = ["person", "Name", "e-mail", "Role", "Professional History", "Passions", "Favorite Books", "Spoken Languages"]
+const tableRow1Data = 
+[
+    "Pe00003",
+    "Mariana",
+    "marianamv112@gmail.com",
+    "CodeOfficer",
+    "Master's in Telecommunications and Informatics Eng., 7months web dev internship, 5 months data internship, Web Dev Bootcamp",
+    "Music (I sing and am learning to play the ukelele), cinema, cultural activities",
+    "The Picture of Dorian Gray",
+    "Portuguese, English"
+]
+
+const tableRow2Data = 
+[
+    "Pe00004",
+    "Stefan",
+    "culaficstefan@gmail.com",
+    "CodeOfficer",
+    "BSc of Informational Systems and Technologies., 3 years IT Admin, 2 years IT Support Eng, Web Dev Bootcamp",
+    "Outdoorsy activities(Hiking, Biking, etc), Reading, Tech",
+    "The Alchemist, Paulo Coelho",
+    "Croatian, English"
+]
+
 const personHead = 
     `<tr>
-        <th>person</th>
-        <th>Name</th>
-        <th>e-mail</th>
-        <th>Role</th>
-        <th>Professional History</th>
-        <th>Passions</th>
-        <th>Favorite Books</th>
-        <th>Spoken Languages</th>
+        <th>${tableHeadData[0]}</th>
+        <th>${tableHeadData[1]}</th>
+        <th>${tableHeadData[2]}</th>
+        <th>${tableHeadData[3]}</th>
+        <th>${tableHeadData[4]}</th>
+        <th>${tableHeadData[5]}</th>
+        <th>${tableHeadData[6]}</th>
+        <th>${tableHeadData[7]}</th>
     </tr>`
 const tableRow1 = 
     `<tr>
-        <td>Pe00003</td>
-        <td>Mariana</td>
-        <td>marianamv112@gmail.com</td>
-        <td>CodeOfficer</td>
-        <td>Master's in Telecommunications and Informatics Eng., 7months web dev internship, 5 months data internship, Web Dev Bootcamp</td>
-        <td>Music (I sing and am learning to play the ukelele), cinema, cultural activities</td>
-        <td>The Picture of Dorian Gray</td>
-        <td>Portuguese, English</td>
+        <td>${tableRow1Data[0]}</td>
+        <td>${tableRow1Data[1]}</td>
+        <td>${tableRow1Data[2]}</td>
+        <td>${tableRow1Data[3]}</td>
+        <td>${tableRow1Data[4]}</td>
+        <td>${tableRow1Data[5]}</td>
+        <td>${tableRow1Data[6]}</td>
+        <td>${tableRow1Data[7]}</td>
     </tr>`
 
 const tableRow2 = 
     `<tr>
-        <td>Pe00004</td>
-        <td>Stefan</td>
-        <td>culaficstefan@gmail.com</td>
-        <td>CodeOfficer</td>
-        <td>BSc of Informational Systems and Technologies., 3 years IT Admin, 2 years IT Support Eng, Web Dev Bootcamp</td>
-        <td>Outdoorsy activities(Hiking, Biking, etc), Reading, Tech</td>
-        <td>The Alchemist, Paulo Coelho</td>
-        <td>Croatian, English</td>
+        <td>${tableRow2Data[0]}</td>
+        <td>${tableRow2Data[1]}</td>
+        <td>${tableRow2Data[2]}</td>
+        <td>${tableRow2Data[3]}</td>
+        <td>${tableRow2Data[4]}</td>
+        <td>${tableRow2Data[5]}</td>
+        <td>${tableRow2Data[6]}</td>
+        <td>${tableRow2Data[7]}</td>
     </tr>`
+
+
 
 function fillTableHead() {
     //Fill rows with data from strings
     //getElementsByTagName and getElementsByClassName returns array hence we have to select first element of the array
+    for(let i = 0; i < personHead.length; i++) {
+
+    }
     document.getElementsByTagName('thead')[0].innerHTML += personHead;
     document.getElementsByClassName('tbody-class')[0].innerHTML += tableRow1;
     document.getElementById('tbody-id').innerHTML += tableRow2;
