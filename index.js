019c02d0aaa3bfb90090de0b9cd83e1af411900c
cule219 <culaficstefan@gmail.com>
2019-08-13 10:58:11 +0200

    separated data file from functions file

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 function fillTable(tableHeadData, tableRowsData) {
-    //Fill thead with data from strings
-
+    //Fill thead with data from nested array
     let tableRow = '';
     tableHeadData.forEach(function(element) {
         tableRow += `<th>${element}</th>`;
     });
-    let thead = document.getElementById('thead-person');
-    thead.innerHTML = `<tr>${tableRow}</tr>`
+    document.getElementById('thead-person').innerHTML += `<tr>${tableRow}</tr>`
 
-    //get tbody element
-    let tbody = document.getElementById('tbody-person');
-    //reset the html within it
-    tbody.innerHTML = '';
-    //load data into tbody
+    //Fill tbody with data from nested array
     tableRowsData.forEach(function(tableRowData) {
         let tableRow = '';
         tableRowData.forEach(function(element) {
             tableRow += `<td>${element}</td>`
         })
-        tbody.innerHTML += `<tr>${tableRow}</tr>`;
+        document.getElementById('tbody-person').innerHTML += `<tr>${tableRow}</tr>`;
     });
 }
-
 fillTable(tableHeadData[0], tableRowsData[0]);
